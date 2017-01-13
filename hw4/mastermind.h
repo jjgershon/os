a98dd9f4767c7760f99d59b84d635c0fdcbab6f7
jjgershon <yonatangershon@gmail.com>
2017-01-13 12:20:03 +0200

    added mastermind.h

---
+++ hw4/mastermind.h
@@ -0,0 +1,47 @@
+#ifndef _MASTERMIND_H_
+#define _MASTERMIND_H_
+
+#include <linux/ioctl.h>
+
+#define MASTERMIND_MAGIC 	'4'
+#define ROUND_START			_IOW(MASTERMIND_MAGIC, 0, int)
+#define GET_MY_SCORE		_IOW(MASTERMIND_MAGIC, 1, int)
+
+int generateFeedback(char* resultBuf, const char* guessBuf, const char* codeBuf){
+	int resultBufIndex=0;
+	int guessBufPaired[4];
+	int codeBufPaired[4];
+	for (int i=0 ; i<4 ; i++) {
+		guessBufPaired[i] = 0;
+		codeBufPaired[i] = 0;
+	}
+	for (int i=0 ; i<4 ; i++) {
+		if (guessBuf[i] == codeBuf[i]) {
+			resultBuf[resultBufIndex] = '2';
+			resultBufIndex++;
+			guessBufPaired[i] = 1;
+			codeBufPaired[i] = 1;
+		}
+	}
+	if (resultBufIndex == 4) {
+		return 1;
+	}
+	for (int i=0 ; i<4 ; i++) {
+		if (!guessBufPaired[i]) {
+			for (int j=0 ; j<4 ; j++) {
+				if (!codeBufPaired[j] && guessBuf[i]==codeBuf[j]) {
+					resultBuf[resultBufIndex] = '1';
+					resultBufIndex++;
+					guessBufPaired[i] = 1;
+					codeBufPaired[j] = 1;
+				}
+			}
+		}
+	}
+	for ( ; resultBufIndex<4 ; resultBufIndex++) {
+		resultBuf[resultBufIndex] = '0';
+	}
+	return 0;
+}
+
+#endif
